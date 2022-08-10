0276cb0ca4ed5cd5a6a711be9b2fd39062135200
mame0124 <mame0124.rkn@gmail.com>
2022-08-10 15:36:51 +0900

    wcコマンド追加

---
+++ 06.wc/wc.rb
@@ -0,0 +1,42 @@
+# frozen_string_literal: true
+
+require 'optparse'
+
+def wc
+  opt = OptionParser.new
+  @options = {}
+  opt.on('-l') { |v| @options[:l] = v }
+  opt.on('-w') { |v| @options[:w] = v }
+  opt.on('-c') { |v| @options[:c] = v }
+  opt.parse!(ARGV)
+
+  file_names = ARGV if ARGV[1]
+  file_name = ARGV[0] if ARGV[0] && ARGV[1].nil?
+
+  if file_name
+    file_info_output(file_name)
+  elsif file_names
+    file_names.each do |filename|
+      file_info_output(filename)
+    end
+  else
+    txt = $stdin.read
+    display(txt)
+    print "\n"
+  end
+end
+
+def file_info_output(file_name)
+  txt = File.read(file_name)
+  display(txt)
+  print " #{file_name}"
+  print "\n"
+end
+
+def display(txt)
+  print format('%8d', txt.lines.count) if @options[:l] || @options.empty?
+  print format('%8d', txt.split.size) if @options[:w] || @options.empty?
+  print format('%8d', txt.size) if @options[:c] || @options.empty?
+end
+
+wc
